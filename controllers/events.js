088e56a89fd0bb416f68c594a002b2dd97eba4d8
Gustavo Ortega <Gustavo@Gustavos-MacBook-Air.local>
2019-04-28 05:02:08 -0700

    added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -19,7 +19,10 @@ app.get('/events/new', (req, res) => {
 });
 
 app.get('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then((event) => {
+    models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then((event) => {
+    let createdAt = event.createdAt;
+    createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+    event.createdAtFormatted = createdAt;
     res.render('events-show', { event: event })
   }).catch((err) => {
     console.log(err.message);
